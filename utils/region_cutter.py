0d9eff2e1e4240bc63c199defc85e19305288a43
joseluisvaz <joseluis.vaz.e@gmail.com>
2018-03-09 20:55:04 -0600

    Update cut dimensions in Region Cutter

--- utils/region_cutter.py
+++ utils/region_cutter.py
@@ -20,13 +20,13 @@ class RegionCutter(object):
             raise ValueError
 
         left_bottom = (100, self.shape[0])
-        right_bottom = (self.shape[1] - 100, self.shape[0])
-        apex1 = (610, 160)
-        apex2 = (680, 160)
-        inner_left_bottom = (200, self.shape[0])
-        inner_right_bottom = (1000, self.shape[0])
-        inner_apex1 = (610,355)
-        inner_apex2 = (680,355)
+        right_bottom = (self.shape[1] - 20, self.shape[0])
+        apex1 = (610, 410)
+        apex2 = (680, 410)
+        inner_left_bottom = (310, self.shape[0])
+        inner_right_bottom = (1150, self.shape[0])
+        inner_apex1 = (700, 480)
+        inner_apex2 = (650, 480)
         self.vertices = np.array([[left_bottom, apex1, apex2,
                                    right_bottom, inner_right_bottom,
                                    inner_apex1, inner_apex2, inner_left_bottom]],
